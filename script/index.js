bc70a08c3654f9bf7ccde68e154d99af832958d0
Dragonir44 <dragonir44@gmail.com>
2022-06-27 16:12:56 +0200

    add content

--- script/index.js
+++ script/index.js
@@ -1,11 +1,13 @@
-let list = document.querySelectorAll('li');
-let content = document.querySelectorAll('.content')
+let list = document.getElementsByClassName('list');
+let content = document.querySelectorAll('.content');
+console.log(list[0])
 for (let i=0; i<list.length; i++){
     list[i].onclick = function(){
         let j = 0;
         while (j < list.length){
             list[j++].className = 'list';
         }
+        console.log(list[i])
         list[i].className = 'list active';
 
         let g = 0;
